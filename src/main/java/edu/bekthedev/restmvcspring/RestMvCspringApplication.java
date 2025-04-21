8956fc727c8be2e4445a906aa8653aeeb69bbd96
Bek Johansson <beksaylor@gmail.com>
2025-04-21 11:09:13 -0500

    first commit

---
+++ src/main/java/edu/bekthedev/restmvcspring/RestMvCspringApplication.java
@@ -0,0 +1,13 @@
+package edu.bekthedev.restmvcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMvCspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestMvCspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/bekthedev/restmvcspring/RestMvCspringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bekthedev.restmvcspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMvCspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
